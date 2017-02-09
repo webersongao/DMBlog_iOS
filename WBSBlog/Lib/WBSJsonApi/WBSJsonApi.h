b0671de0368b3e8f69353e69ccfab4d766471713
Gao Lishun <gaolishun@zongheng.com>
2017-02-09 23:58:44 +0800

    修改网络请求
    修改网络请求

--- WBSBlog/Lib/WBSJsonApi/WBSJsonApi.h
+++ WBSBlog/Lib/WBSJsonApi/WBSJsonApi.h
@@ -3,20 +3,24 @@
 //  WBSJsonApi
 //
 //  Created by Weberson on 2017/1/26.
-//  该API 需要WordPress安装JsonApi 插件 涉及到”用户信息“以及“用户登陆” 还需要Json Api-User 插件支持
+//  该API 需要WordPress 的 Json Api-User 插件支持
 //  Copyright © 2017年 Weberson. All rights reserved.
 //
-// 该文件主要包含 【博客用户】 的 注册 删除 修改 搜索 等操作，【博客文章】的 增 删 改 查 参看 WBSPostJsonApi 类
+// 该文件主要包含 【博客用户】 的 注册 删除 修改 搜索 等操作，【博客文章】的 增 删 改 查 参看 WBSBaseCoreApi 类
 
-#import "WBSPostJsonApi.h"
+#import "WBSBaseCoreApi.h"
 
-@interface WBSJsonApi : WBSPostJsonApi
 
-//  1、User  需要 JsonApi User 插件支持  https://wordpress.org/plugins/json-api-user/
-#pragma mark  About User
+@interface WBSJsonApi : WBSBaseCoreApi
+
+
+/**
+ *  1、User  需要 Json Api User 插件支持  https://wordpress.org/plugins/json-api-user/
+ */
+#pragma mark  1️⃣   About User
 
 /// 用户登陆 Login
-+ (void)userLoginWithURL:(NSString *)siteUrl username:(NSString *)userName password:(NSString *)password insecure:(BOOL)insecure success:(void (^)(NSDictionary *resultDict))success failure:(void (^)(NSError *error))failure;
++ (void)userLoginWithURL:(NSString *)siteUrl username:(NSString *)userName password:(NSString *)password inSSLSecure:(BOOL)inSSLsecure success:(void (^)(NSDictionary *resultDict))success failure:(void (^)(NSError *error))failure;
 
 
 
@@ -30,10 +34,6 @@
 
 
 
-
-
-//  2、Post / Setting  需要JsonApi 插件支持  https://wordpress.org/plugins/json-api/
-// 具体代码实现 参看 WBSPostJsonApi.h 和 WBSPostJsonApi.m
 
 
 @end
--- WBSBlog/Utils/NetworkingCenter.h
+++ WBSBlog/Utils/NetworkingCenter.h
@@ -12,7 +12,7 @@
 #import <Foundation/Foundation.h>
 #import "AFNetworking.h"
 
-@interface NetworkingCenter : AFHTTPRequestOperationManager
+@interface NetworkingCenter : AFHTTPSessionManager
 
 /*-------------------- 加header的 get OR post请求 -----------------*/
 /**
@@ -24,7 +24,7 @@
  *  @param noLigin    用户未登录的处理
  *  @param failure    网络请求失败
  */
-+ (AFHTTPRequestOperation *)GetWithHeader:(NSString *)urlString parameters:(id)parameters isShowHUB:(BOOL)show success:(void (^)(id responseObject))success noLogin:(void(^)())noLigin failure:(void (^)(NSError *error))failure;
++ (NSURLSessionDataTask *)GetWithHeader:(NSString *)urlString parameters:(id)parameters isShowHUB:(BOOL)show success:(void (^)(id responseObject))success noLogin:(void(^)())noLigin failure:(void (^)(NSError *error))failure;
 
 /**
  *  带有sk的网络请求(POST方式)
@@ -35,22 +35,21 @@
  *  @param noLigin    用户未登录的处理
  *  @param failure    网络请求失败
  */
-//+ (AFHTTPRequestOperation *)PostWithHeader:(NSString *)urlString parameters:(id)parameters success:(void (^)(id responseObject))success noLogin:(void(^)())noLigin failure:(void (^)(NSError *error))failure;
-+ (AFHTTPRequestOperation *)PostWithHeader:(NSString *)urlString parameters:(id)parameters isShowHUB:(BOOL)show success:(void (^)(id responseObject))success noLogin:(void(^)())noLigin failure:(void (^)(NSError *error))failure;
++ (NSURLSessionDataTask *)PostWithHeader:(NSString *)urlString parameters:(id)parameters isShowHUB:(BOOL)show success:(void (^)(id responseObject))success noLogin:(void(^)())noLigin failure:(void (^)(NSError *error))failure;
 
 
 /*-------------------- 不加header  get OR post请求  -------------------*/
-+ (AFHTTPRequestOperation *)GETRequest:(NSString *)URLString parameters:(id)parameters success:(void (^)(id responseObject))success failure:(void (^)(NSError *error))failure;
++ (NSURLSessionDataTask *)GETRequest:(NSString *)URLString parameters:(id)parameters success:(void (^)(id responseObject))success failure:(void (^)(NSError *error))failure;
 
-+ (AFHTTPRequestOperation *)POSTRequest:(NSString *)URLString parameters:(id)parameters success:(void (^)(id responseObject))success failure:(void (^)(NSError *error))failure;
++ (NSURLSessionDataTask *)POSTRequest:(NSString *)URLString parameters:(id)parameters success:(void (^)(id responseObject))success failure:(void (^)(NSError *error))failure;
 
 /*---------------- 方便取消下载任务 ----------------*/
-+ (AFHTTPRequestOperation *)GET:(NSString *)URLString
++ (NSURLSessionDataTask *)GET:(NSString *)URLString
                      parameters:(id)parameters
                         success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success
                         failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
 
-+ (AFHTTPRequestOperation *)POST:(NSString *)URLString
++ (NSURLSessionDataTask *)POST:(NSString *)URLString
                      parameters:(id)parameters
                          success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success
                          failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
